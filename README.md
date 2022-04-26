5b30f8d095709f74601f07bff67e5a4a769cd5de
primesho <s.osaka@primecast.co.jp>
2022-04-26 13:13:20 +0900

    READMEの変更2

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # MQTTテストサーバー
+- OS: Ubuntu
 
 ##
+```commandline
+sudo apt update
+sudo apt upgrade
+sudo apt-get install mosquitto
+```
+
+```commandline
+pip install -r requirements.txt
+```
--- mqtt_server.py
+++ mqtt_server.py
@@ -1,41 +1,28 @@
-#!usr/bin/env python
-# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+
+host = '127.0.0.1'
+port = 1883
+topic = 'tp1/sub1'
+topic2 = 'tp1/sub2'
 
 
-import paho.mqtt.client as mqtt     # MQTTのライブラリをインポート
-from time import sleep              # 3秒間のウェイトのために使う
+def on_connect(client, userdata, flags, respons_code):
+    print('status {0}'.format(respons_code))
+    # topicを複数登録してみた
+    client.subscribe(topic)
+    client.subscribe(topic2)
 
-# ブローカーに接続できたときの処理
-def on_connect(client, userdata, flag, rc):
-    print("Connected with result code " + str(rc))
 
-# ブローカーが切断したときの処理
-def on_disconnect(client, userdata, flag, rc):
-    if rc != 0:
-        print("Unexpected disconnection.")
+def on_message(client, userdata, msg):
+    # topic QOS payloadを取得
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-# publishが完了したときの処理
-def on_publish(client, userdata, mid):
-    print("publish: {0}".format(mid))
 
-# メイン関数   この関数は末尾のif文から呼び出される
-def main(debug=False):
-    client = mqtt.Client()                 # クラスのインスタンス(実体)の作成
-    client.on_connect = on_connect         # 接続時のコールバック関数を登録
-    client.on_disconnect = on_disconnect   # 切断時のコールバックを登録
-    client.on_publish = on_publish         # メッセージ送信時のコールバック
-
-    client.connect("localhost", 1883, 60)  # 接続先は自分自身
-
-    # 通信処理スタート
-    client.loop_start()    # subはloop_forever()だが，pubはloop_start()で起動だけさせる
-
-    # 自分自身で起動[デバッグモード]
-    if debug:
-        # 永久に繰り返す
-        while True:
-            client.publish("drone/001", "Hello, Drone!")    # トピック名とメッセージを決めて送信
-            sleep(3)   # 3秒待つ
-
-if __name__ == '__main__': # importされないときだけmain()を呼ぶ
-    main(True)# メイン関数を呼び出す
+if __name__ == '__main__':
+    # Publisherと同様に v3.1.1を利用
+    client = mqtt.Client(protocol=mqtt.MQTTv311)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(host, port=port, keepalive=60)
+    # 待ち受け状態にする
+    client.loop_forever()
